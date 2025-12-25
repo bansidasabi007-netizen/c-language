1896e1ea46d3c84de05f7c1fccef495996867ba3
bansidasabi007-netizen <bansidasabi007@gmail.com>
2025-12-25 12:01:00 +0530

    Add files via upload

---
+++ pattern.c
@@ -0,0 +1,20 @@
+#include<stdio.h>
+
+
+int main(){
+    // int i,j ;
+    // for(int i = 41; i<=45; i++ ){
+    //     for(int j=41; j<=i; j++){
+    //         printf("%d ",j );
+    //     }printf("\n");
+    // }
+
+
+    int  count=11 ;
+    for(int k=5 ; k > 1 ; k--){
+        for(int l=k; l<=5; l++){
+            printf("%d ",count);
+            count++;
+        }printf("\n");
+    }
+}
