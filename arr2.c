4ea231cd747dcc206b36bef88fc908d6d575d587
bansidasabi007-netizen <bansidasabi007@gmail.com>
2026-01-02 11:24:08 +0530

    array.c

---
+++ arr2.c
@@ -0,0 +1,112 @@
+#include <stdio.h>
+int main() {
+
+//   int size;
+//   printf("Enter The Size of array : ");
+//   scanf("%d", &size);
+//  int arr[size];
+
+//  for(int i = 0; i<size; i++){
+//     printf("Enter the value of : ");
+//     scanf("%d", &arr[i]);
+//  }
+//  printf("Negative value of Element:");
+//   for(int i = 0; i<size; i++){
+//    if(arr[i]<0){
+//       printf("%d ", arr[i]);
+//    }
+   
+//   }
+
+
+
+
+
+// int rows,cols,m,n,max;
+// printf("Enter the array's row size:");
+// scanf("%d",&rows);
+// printf("Enter the array's column size:");
+// scanf("%d",&cols);
+// int a[rows][cols];
+// printf("\nEnter array's elements\n");
+// for(m=0; m<rows; m++){
+//     for(n=0; n<cols; n++){
+//         printf("a[%d][%d]=",m,n);
+//         scanf("%d",&a[m][n]);
+//     }
+// }
+// max=a[0][0];
+// for(m=0;m<rows;m++){
+//     for(n=0;n<cols;n++){
+//         if(a[m][n]>max){
+//             max=a[m][n];
+//         }
+//     }
+// }
+// printf("\nThe largest element is:%d\n",max);
+
+
+
+
+
+
+// int size,o,p;
+// printf("Enter the array's row & column size: ");
+// scanf("%d ",&size);
+// int a[size][size];
+// printf("Enter array's elements:\n");
+// for(o=0;o<size;o++){
+//     for(p=0;p<size;p++){
+//         printf("a[%d][%d]=",o,p);
+//         scanf("%d",&a[o][p]);
+//     }
+// }
+// printf("\nThe transpose matrix of an array:\n");
+// for(o=0;o<size;o++){
+//     for(p=0;p<size;p++){
+//         printf("%d",a[o][p]);
+//     }
+//     printf("\n");
+// }
+
+
+
+
+
+//  int rows, cols, i, j, r_num, c_num, r_sum = 0, c_sum = 0;
+
+//     printf("Enter the array's row size: ");
+//     scanf("%d", &rows);
+//     printf("Enter the array's column size: ");
+//     scanf("%d", &cols);
+
+//     int a[rows][cols];
+
+//     printf("Enter array's elements:\n");
+//     for (i = 0; i < rows; i++) {
+//         for (j = 0; j < cols; j++) {
+//             printf("a[%d][%d] = ", i, j);
+//             scanf("%d", &a[i][j]);
+//         }
+//     }
+
+//     printf("\nEnter row number: ");
+//     scanf("%d", &r_num);
+//     printf("Elements of row %d: ", r_num);
+//     for (j = 0; j < cols; j++) {
+//         printf("%d%s", a[r_num][j], (j == cols - 1) ? "" : ", ");
+//         r_sum += a[r_num][j];
+//     }
+//     printf("\nThe sum of a row %d: %d\n", r_num, r_sum);
+
+  
+//     printf("\nEnter column number: ");
+//     scanf("%d", &c_num);
+//     printf("Elements of column %d: ", c_num);
+//     for (i = 0; i < rows; i++) {
+//         printf("%d%s", a[i][c_num], (i == rows - 1) ? "" : ", ");
+//         c_sum += a[i][c_num];
+//     }
+//     printf("\nThe sum of column %d: %d\n", c_num, c_sum);
+
+}
